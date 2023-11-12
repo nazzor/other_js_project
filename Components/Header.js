1ff8582e53089d1ce634e3c3959ea9b25be7d9aa
Krenar Miftari <krenaar@live.com>
2023-11-12 20:20:42 +0100

    final

--- Components/Header.js
+++ Components/Header.js
@@ -2,22 +2,20 @@ export function initHeader(div){
   div.innerHTML = `
   <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a class="navbar-brand" href="#">Car Parts</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto me-5">
-          <li class="d-flex align-items-center">
-            <form class="d-block align-items-center" role="search">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
+        <a class="navbar-brand" href="#">Car Parts</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="d-flex ms-auto" role="search">
+              <input id="search-input" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             </form>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Menu
-            </a>
-            <ul class="dropdown-menu">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Menu
+                </a>
+            <ul class="dropdown-menu dropdown-menu-end">
               <li><a class="dropdown-item" href="#">Dashboard</a></li>
               <li><a class="dropdown-item" href="#">Login</a></li>
               <li><hr class="dropdown-divider" /></li>
